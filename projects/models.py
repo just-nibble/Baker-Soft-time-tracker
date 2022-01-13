d0597b22a387a2f7b8938c7e0520f701bb0dcbe2
Princewill Olaiya <princewillolaiya@gmail.com>
2022-01-13 14:50:59 +0100

    basic model for projects and tasks

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255, null=True)
+    manager = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    start_time = models.DateTimeField(auto_now_add=True)
+    projected_completion_time = models.DateTimeField(null=True)
+    actual_completion_time = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    status_options = (("pending", "pending"), ("assigned",
+                      "assigned"), ("completed", "completed"))
+    title = models.CharField(max_length=255, null=True)
+    description = models.TextField(max_length=255, null=True)
+    attachment = models.FileField(
+        upload_to="task/attachment", null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
+    status = models.CharField(max_length=15, null=True, choices=status_options)
+
+    def __str__(self):
+        return self.title
+
+
+class ProjectMembers(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, null=True)
+    member = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return str(self.task.title) + ": " + str(self.member.username)
