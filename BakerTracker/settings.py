27615d970efcfb8f2a4d5be96d4c91c3e0d85923
Princewill Olaiya <princewillolaiya@gmail.com>
2022-01-13 14:11:34 +0100

    this commit records the creation of the custom user model

--- BakerTracker/settings.py
+++ BakerTracker/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # User Created apps
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'BakerTracker.urls'
 
