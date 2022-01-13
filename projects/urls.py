db168b3bbafc65d6063c98ec5efe963b2d2bc787
Princewill Olaiya <princewillolaiya@gmail.com>
2022-01-13 17:41:13 +0100

    added names to path

--- projects/urls.py
+++ projects/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.ProjectAPIView.as_view()),
-    path("<int:pk>/", views.ProjectDetailAPIView.as_view()),
-    path("task/", views.TaskAPIView.as_view()),
-    path("task/<int:pk>/", views.TaskDetailAPIView.as_view()),
-    path("assign/", views.ProjectMemberAPIView.as_view()),
-    path("assign/<int:pk>/", views.ProjectMemberDetailAPIView.as_view()),
+    path("", views.ProjectAPIView.as_view(), name="project"),
+    path("<int:pk>/", views.ProjectDetailAPIView.as_view(), name="project_detail"),
+    path("task/", views.TaskAPIView.as_view(), name="task"),
+    path("task/<int:pk>/", views.TaskDetailAPIView.as_view(), name="task_detail"),
+    path("assign/", views.ProjectMemberAPIView.as_view(), name="assign"),
+    path("assign/<int:pk>/", views.ProjectMemberDetailAPIView.as_view(),
+         name="assign_detail"),
 ]
